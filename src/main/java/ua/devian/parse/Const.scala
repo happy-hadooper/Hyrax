76242817fc876312ca7850786b58ca7909da0aa4
Artur Sukhenko <asukhenko@cybervisiontech.com>
2016-07-19 19:09:28 +0300

    Const with constants

---
+++ src/main/java/ua/devian/parse/Const.scala
@@ -0,0 +1,9 @@
+package ua.devian.parse
+
+/**
+  * Created by devian on 15.07.16.
+  */
+object Const {
+  val CLASS_CONTAINER_IMPL = "RMContainerImpl".intern()
+  val CLASS_FAIR_SCHEDULER = "FairScheduler".intern()
+}
