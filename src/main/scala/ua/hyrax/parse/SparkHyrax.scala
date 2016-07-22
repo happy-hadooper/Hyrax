feaf0ff70adb66847d575e3875dbcdb114257627
Artur Sukhenko <artur.sukhenko@gmail.com>
2016-07-22 12:59:55 +0300

    Check for empty args. Allow multiple files.

--- src/main/scala/ua/hyrax/parse/SparkHyrax.scala
+++ src/main/scala/ua/hyrax/parse/SparkHyrax.scala
@@ -3,16 +3,19 @@ package ua.hyrax.parse
 /**
   * Created by devian on 14.07.16.
   */
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{SparkConf, SparkContext, rdd}
 import ua.hyrax.parse.util.Print
 
 object SparkHyrax extends Serializable{
 
   def main(args: Array[String]): Unit = {
+    if(args.isEmpty){
+      println("Add files to run this app.")
+      sys.exit(1)
+    }
     val conf = new SparkConf().setAppName("Hyrax").setMaster("yarn-client")
     val sc = new SparkContext(conf)
-    //val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    val logsFile = sc.textFile(args(0))
+    val logsFile = args.map(path => sc.textFile(path)).reduce(_ ++ _)
     Print.transitionInfo(logsFile)
 
   }
