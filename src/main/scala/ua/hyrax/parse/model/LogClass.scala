cb2d0bf43360035849f50ede0e10214166becec6
Artur Sukhenko <artur.sukhenko@gmail.com>
2016-07-28 17:31:16 +0300

    LogClass enum

---
+++ src/main/scala/ua/hyrax/parse/model/LogClass.scala
@@ -0,0 +1,14 @@
+package ua.hyrax.parse.model
+
+/**
+  * Created by devian on 22.07.16.
+  */
+object LogClass extends Enumeration{
+  type LogClass = Value
+  val RMContainerImpl= Value("RMContainerImpl")
+  val FairScheduler = Value("FairScheduler")
+  val Null = Value("Null")
+
+
+}
+
